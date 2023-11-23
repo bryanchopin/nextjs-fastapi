b98befba75b4f90867ee07f4e1144b270e712919
bryanchopin <bryan.oliveros17@tectijuana.edu.mx>
2023-11-22 21:50:09 -0800

    Refactor WebSocket implementation and integrate Pusher

--- api/index.py
+++ api/index.py
@@ -44,16 +44,66 @@
 
 
 
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from typing import Set
-import pusher
+# from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+# from typing import Set
+# import pusher
 
+
+# app = FastAPI()
+
+# # Conjunto para almacenar conexiones WebSocket activas
+# websocket_connections: Set[WebSocket] = set()
+
+# pusher_client = pusher.Pusher(
+#   app_id='1713056',
+#   key='54b9980aed66a1f270b8',
+#   secret='ea8862abeaf2572fb16c',
+#   cluster='us3',
+#   ssl=True
+# )
+
+# @app.get("/api/python")
+# def hello_world():
+#     return {"message": "Hello World"}
+
+# # @app.websocket("/ws")
+# # async def websocket_endpoint(websocket: WebSocket):
+# #     await websocket.accept()
+# #     websocket_connections.add(websocket)
+
+# #     try:
+# #         while True:
+# #             data = await websocket.receive_text()
+
+# #             # Retransmitir el mensaje a todos los clientes conectados
+# #             for connection in websocket_connections:
+# #                 await connection.send_text(f"Cliente {websocket.client.host}: {data}")
+
+# #     except WebSocketDisconnect:
+# #         # Eliminar la conexión si se desconecta
+# #         websocket_connections.remove(websocket)
+
+
+# @app.websocket("/ws")
+# async def websocket_endpoint(websocket: WebSocket):
+#     await websocket.accept()
+
+#     try:
+#         while True:
+#             data = await websocket.receive_text()
+#             pusher.trigger('my-channel', 'my-event', {'message': data})
+
+#     except WebSocketDisconnect:
+#         pass
+
+
+
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from pusher import Pusher
 
 app = FastAPI()
 
-# Conjunto para almacenar conexiones WebSocket activas
-websocket_connections: Set[WebSocket] = set()
-
+# Configura las credenciales de Pusher
 pusher_client = pusher.Pusher(
   app_id='1713056',
   key='54b9980aed66a1f270b8',
@@ -62,36 +112,21 @@ pusher_client = pusher.Pusher(
   ssl=True
 )
 
-@app.get("/api/python")
-def hello_world():
-    return {"message": "Hello World"}
-
-# @app.websocket("/ws")
-# async def websocket_endpoint(websocket: WebSocket):
-#     await websocket.accept()
-#     websocket_connections.add(websocket)
-
-#     try:
-#         while True:
-#             data = await websocket.receive_text()
-
-#             # Retransmitir el mensaje a todos los clientes conectados
-#             for connection in websocket_connections:
-#                 await connection.send_text(f"Cliente {websocket.client.host}: {data}")
-
-#     except WebSocketDisconnect:
-#         # Eliminar la conexión si se desconecta
-#         websocket_connections.remove(websocket)
-
+# Lista para almacenar conexiones WebSocket activas
+websocket_connections = set()
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
+    websocket_connections.add(websocket)
 
     try:
         while True:
             data = await websocket.receive_text()
+
+            # Retransmitir el mensaje a todos los clientes a través de Pusher
             pusher.trigger('my-channel', 'my-event', {'message': data})
 
     except WebSocketDisconnect:
-        pass
+        # Eliminar la conexión si se desconecta
+        websocket_connections.remove(websocket)
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,122 @@
+// 'use client';
+// import Image from "next/image";
+// import Link from "next/link";
+// import { useEffect, useState } from "react";
+// import Pusher from 'pusher-js';
+
+// export default function Home() {
+//   const [messages, setMessages] = useState<string[]>([]);
+//   const [inputMessage, setInputMessage] = useState('');
+
+//   useEffect(() => {
+//     const pusher = new Pusher('54b9980aed66a1f270b8', {
+//       cluster: 'us3'
+//     });
+
+//     const channel = pusher.subscribe('my-channel');
+//     channel.bind('my-event', function(data: any) {
+//       alert(JSON.stringify(data));
+//     });
+//   });
+
+//   useEffect(() => {
+//     // const socket = new WebSocket("ws://localhost:8000/ws");
+//     const socket = new WebSocket("wss://nextjs-fastapi-amber.vercel.app/ws");
+
+
+//     // Manejar eventos
+//     socket.onopen = (event) => {
+//       console.log("Conexión WebSocket abierta:", event);
+//     };
+
+//     socket.onmessage = (event) => {
+//       const receivedMessage = event.data;
+//       setMessages((prevMessages) => [...prevMessages, receivedMessage]);
+//     };
+
+//     socket.onclose = (event) => {
+//       console.log("Conexión WebSocket cerrada:", event);
+//       console.log("Razón del cierre:", event.reason);
+//     };
+
+//     // Limpiar la conexión WebSocket cuando el componente se desmonta
+//     return () => {
+//       socket.close();
+//     };
+//   }, []);
+//   const sendMessage = () => {
+//     // const socket = new WebSocket("ws://localhost:8000/ws");
+//     const socket = new WebSocket("wss://nextjs-fastapi-amber.vercel.app/ws");
+
+
+//     // Esperar a que la conexión esté abierta antes de enviar el mensaje
+//     socket.onopen = () => {
+//       socket.send(inputMessage);
+//       setInputMessage('');
+//     };
+
+//     // Manejar eventos adicionales si es necesario
+//     socket.onclose = (event) => {
+//       console.log("Conexión WebSocket cerrada:", event);
+//     };
+//   };
+  
+
+//   return (
+//     <div className="flex flex-col items-center justify-center p-24">
+//       <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100">
+//         <h2 className={`mb-3 text-2xl font-semibold`}>Chat</h2>
+//         <div className="flex flex-col max-w-[30ch] text-sm">
+//           <div
+//             className="mb-2 overflow-auto max-h-40"
+//             style={{ borderBottom: "1px solid #ccc" }}
+//           >
+//             {messages.map((message, index) => (
+//               <div key={index}>{message}</div>
+//             ))}
+//           </div>
+//           <input
+//             type="text"
+//             placeholder="Type your message..."
+//             value={inputMessage}
+//             onChange={(e) => setInputMessage(e.target.value)}
+//             className="border border-gray-300 p-2"
+//           />
+//           <button
+//             onClick={sendMessage}
+//             className="mt-2 bg-blue-500 text-white px-4 py-2 rounded"
+//           >
+//             Send
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+
+//   // useEffect(() => {
+//   //   const socket = new WebSocket("ws://localhost:8000/ws");
+
+//   //   socket.onmessage = (event) => {
+//   //     const receivedMessage = event.data;
+//   //     setMessages((prevMessages) => [...prevMessages, receivedMessage]);
+//   //   };
+
+//   //   socket.onclose = (event) => {
+//   //     console.log("Conexión WebSocket cerrada:", event);
+//   //   };
+
+//   //   // Limpiar la conexión WebSocket cuando el componente se desmonta
+//   //   return () => {
+//   //     socket.close();
+//   //   };
+//   // }, []);
+
+
 'use client';
-import Image from "next/image";
-import Link from "next/link";
-import { useEffect, useState } from "react";
+
+import { useEffect, useState } from 'react';
 import Pusher from 'pusher-js';
 
 export default function Home() {
@@ -14,53 +129,20 @@ export default function Home() {
     });
 
     const channel = pusher.subscribe('my-channel');
-    channel.bind('my-event', function(data: any) {
-      alert(JSON.stringify(data));
+
+    channel.bind('my-event', function(data : any) {
+      setMessages((prevMessages) => [...prevMessages, data.message]);
     });
-  });
 
-  useEffect(() => {
-    // const socket = new WebSocket("ws://localhost:8000/ws");
-    const socket = new WebSocket("wss://nextjs-fastapi-amber.vercel.app/ws");
-
-
-    // Manejar eventos
-    socket.onopen = (event) => {
-      console.log("Conexión WebSocket abierta:", event);
-    };
-
-    socket.onmessage = (event) => {
-      const receivedMessage = event.data;
-      setMessages((prevMessages) => [...prevMessages, receivedMessage]);
-    };
-
-    socket.onclose = (event) => {
-      console.log("Conexión WebSocket cerrada:", event);
-      console.log("Razón del cierre:", event.reason);
-    };
-
-    // Limpiar la conexión WebSocket cuando el componente se desmonta
     return () => {
-      socket.close();
+      pusher.unsubscribe('my-channel');
+      pusher.disconnect();
     };
   }, []);
+
   const sendMessage = () => {
-    // const socket = new WebSocket("ws://localhost:8000/ws");
-    const socket = new WebSocket("wss://nextjs-fastapi-amber.vercel.app/ws");
-
-
-    // Esperar a que la conexión esté abierta antes de enviar el mensaje
-    socket.onopen = () => {
-      socket.send(inputMessage);
-      setInputMessage('');
-    };
-
-    // Manejar eventos adicionales si es necesario
-    socket.onclose = (event) => {
-      console.log("Conexión WebSocket cerrada:", event);
-    };
+    // Aquí puedes enviar el mensaje al servidor, si es necesario
   };
-  
 
   return (
     <div className="flex flex-col items-center justify-center p-24">
@@ -93,22 +175,3 @@ export default function Home() {
     </div>
   );
 }
-
-
-  // useEffect(() => {
-  //   const socket = new WebSocket("ws://localhost:8000/ws");
-
-  //   socket.onmessage = (event) => {
-  //     const receivedMessage = event.data;
-  //     setMessages((prevMessages) => [...prevMessages, receivedMessage]);
-  //   };
-
-  //   socket.onclose = (event) => {
-  //     console.log("Conexión WebSocket cerrada:", event);
-  //   };
-
-  //   // Limpiar la conexión WebSocket cuando el componente se desmonta
-  //   return () => {
-  //     socket.close();
-  //   };
-  // }, []);
